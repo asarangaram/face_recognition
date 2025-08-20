f0de427f77bf708954a9b716cf2db47018b78a63
Ananda S RPi <4833102+asarangaram@users.noreply.github.com>
2025-08-20 17:22:40 +0530

    added clean switch

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
+import argparse
 from pathlib import Path
 from store_init import init_store
 
 
-class Config:
-    FACE_STORE_DIR = "face_store"
-
 
 if __name__ == "__main__":
-    rebuild_store = True
-    preserve_past = not rebuild_store
-    recogniser = init_store(Config.FACE_STORE_DIR, preserve_past=False)
-    if rebuild_store:
-        path = "/home/anandas/demos/degirum_hailo_examples/assets/Friends_dataset"
-        faces = [
-            (file.stem.split("_")[0], str(file))
-            for file in Path(path).rglob("*")
-            if file.suffix.lower() in (".png", ".jpg", ".jpeg")
-        ]
+    parser = argparse.ArgumentParser(description="Face Store Initializer")
+    parser.add_argument(
+        "--rebuild-store",
+        action="store_true",
+        help="Rebuild the face store from dataset (default: False)",
+    )
+    parser.add_argument(
+        "--face-store-dir",
+        type=str,
+        default=f"{Path.home()}/.local/share/colan_apps/store/face_db",
+        help="Directory to store faces (default: face_store)",
+    )
+    parser.add_argument(
+        "--faces",
+        nargs="+",
+        required=False,
+        help="One or more dataset directories containing face images",
+    )
+    args = parser.parse_args()
 
-        face_ids = recogniser.register_faces_no_batch(faces)
-        print(f"Available faces: {len(faces)}")
-        print(f"Registerred faces: {len(face_ids)}")
+    rebuild_store = args.rebuild_store
+    preserve_past = not rebuild_store
+    recogniser = init_store(args.face_store_dir, preserve_past=preserve_past)
+
+    if rebuild_store and args.faces:
+        all_faces = []
+        for path in args.faces:
+            for file in Path(path).rglob("*"):
+                if file.suffix.lower() in (".png", ".jpg", ".jpeg"):
+                    all_faces.append((file.stem.split("_")[0], str(file)))
+
+        face_ids = recogniser.register_faces_no_batch(all_faces)
+        print(f"Available faces: {len(all_faces)}")
+        print(f"Registered faces: {len(face_ids)}")
